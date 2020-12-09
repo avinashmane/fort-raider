f380e5e00cdb3a9709a586ae5127c42baf89451c
Avinash Mane <avinash.mane@in.ibm.com>
2020-12-09 15:44:26 +0530

    V2 after creation of FB page

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-"# fort-raider" 
-"# fort-raider" 
+# fort-raider
+
+Website that support guide of forts and GPS trail maps
+
+App fort-raider.web.app support GPS mapping of all forts.  List is in progress and increasing gradually.
+
+# Notes
+
+## Fort
+Word fort is loosely defined fort since it does not always mean fortification (defence wall or structure).  Castles and fortified building can very well fit the description just that they may not be called as forts.
+
+### Type of forts
+* Hill forts/Castles: may or may not have walls built
+* Land forts/Castles: fortification is common, but no significant elevation.  Sometimes walled cities will be covered under this
+* Water forts:  Forts in sea, river, lake
+
--- public/forts.js
+++ public/forts.js
@@ -30,24 +30,29 @@
         aurora    = L.marker([17.69, 74]).bindPopup('This is Satara'),
         golden    = L.marker([16.7, 74.23]).bindPopup('This is Kolhapur.');    
 	var cities = L.layerGroup([littleton, denver, aurora, golden]);
-		fms=forts.map((x) => { 
-			return L.marker([x.coord[1], x.coord[0]], {icon: fortIcons[x.category] , title:x.fort})
-			                .bindPopup(x.fort+':'+x.category)}),
-		fortLayer= L.layerGroup( fms )
-		// Cluster
-		var markers = L.markerClusterGroup();
-		for (i in fms) {
-			markers.addLayer(fms[i]);
-		}
-    var grayscale = L.tileLayer(mbUrl, {id: 'mapbox/streets-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
-        streets   = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
-	var OpenTopoMap = L.tileLayer.provider('OpenTopoMap')
+
+	fms=forts.map((x) => { 
+		return L.marker([x.coord[1], x.coord[0]],
+			//  {icon:  L.divIcon({
+			//     className: 'text-labels',   // Set class for CSS styling
+			//     html: '<i class="fa fa-fort-awesome" aria-hidden="true"></i><div class="fort">'+x.fort.replace(" Fort","")+'</div>'}) 
+			// , title:x.fort})
+			{icon: fortIcons[x.category]})
+			.bindPopup(x.fort+':'+x.category)}),
+	fortLayer= L.layerGroup( fms )
+	// Cluster
+	var markers = L.markerClusterGroup();
+	for (i in fms) {
+		markers.addLayer(fms[i]);
+	}
+	
+
+	var  streets = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
 
 	var baseLayers = {
-			"Grayscale": grayscale,
 			"Streets": streets,
-			"Topo":OpenTopoMap,
 			"OSM Mapnik": L.tileLayer.provider('OpenStreetMap.Mapnik'),
+			"Topo":L.tileLayer.provider('OpenTopoMap'),
 			"ESRI Imagery": L.tileLayer.provider('Esri.WorldImagery'),
 			"HikeBike": L.tileLayer.provider('HikeBike.HikeBike'),
 			"HERE.hybridDay": L.tileLayer.provider('HEREv3.hybridDay'),
@@ -58,13 +63,12 @@
     var map = L.map('mapid', {
         center: [18.5204, 73.85],
         zoom: 8,
-        layers: [grayscale, fortLayer]
+        layers: [streets, fortLayer]
     });
 
 	map.addLayer(markers);
 
-    fortIcon='https://upload.wikimedia.org/wikipedia/commons/3/3e/Grey_castle_icon.svg'
-    // a = omnivore.kml('Avinashforts.kml')
+	// a = omnivore.kml('Avinashforts.kml')
     // debugger;
     // var runLayer =a.on('ready', function() {
     //     map.fitBounds(runLayer.getBounds());
@@ -104,8 +108,36 @@
 	// 	[51.503, -0.06],
 	// 	[51.51, -0.047]
 	// ]).addTo(mymap).bindPopup("I am a polygon.");
+/*
+	var pointA = new L.LatLng(18.806817, 74.349976);
+	var pointB = new L.LatLng(18.984461, 73.70641);
+	var pointList = [pointA, pointB];
+	// debugger;
+	encoded='ku|dBkuadMZ_HmQaBcAxAgV{pAiL}cEofFuIibAnNA]}b`@i}@me{Adxc@cuwAp|[_qf@c{J}huBdgv@e~SffkAi_s@hys@iiD`zHyvHv{@wwEzu@iMjq@}d@v~E_SiMqfD_c@kj@fJixB_mAkKuJy}BpEyaA~Aoj@nOkvEnmLw~qAkvCcdnBt`Fge@u_@mjkAwiIk_TacUi__@nnS{ysAtGi~bDywk@qsQaiLqc@o@wZtI}fC`mBaIr^aoZsfm@yxTb~AmeYyxZgiHumNybEhhHqFj[W~@EV{i~AnqF_jb@jnv@mce@kmL{J{_@_kQmb[sk}@yva@ogrAxoB_hOw_JmyFryA}eBaw@_sEwo@majAg`Ysuy@bxPi`pC_rmBu{gAazeAebkAichEwySg`E{ql@}gb@wvZqy@}y_B}{{@evaAoceBqjjAu|gAiR`AgAeBcAsBiaFupCkdA_p@oCz@}y@mk@}QtCmXsJmB_ALUyDoMyQw~@]fH^C' ;
+	pointList= L.PolylineUtil.decode(encoded);
+	// console.log(pointList)
+	var firstpolyline = new L.polyline( pointList , {
+			color: 'red',
+			weight: 3,
+			opacity: 0.5,
+			smoothFactor: 1
+		});
 
+		firstpolyline.addTo(map);
 
+		runner=L.motion.polyline(pointList, {
+			color: "transparent"
+		}, {
+			auto: true,
+			duration: 3000,
+			easing: L.Motion.Ease.easeInOutQuart
+		}, {
+			removeOnEnd: false,
+			showMarker: true,
+			// icon: L.divIcon({html: "<H1>AA</H1><i class='fa fa-car fa-2x' aria-hidden='true'></i>", iconSize: L.point(27.5, 24)})
+		}).addTo(map);
+*/
+/*
 	var popup = L.popup();
 
 	function onMapClick(e) {
@@ -116,4 +148,4 @@
 	}
 
 	map.on('dblclick', onMapClick);
-
+*/
